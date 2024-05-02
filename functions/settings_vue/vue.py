3170b24577d1e57435acbde8deb4f23cdce54205
pyCampaDB <rafacampamartinez@gmail.com>
2024-05-02 21:14:26 +0200

    first commit

--- functions/settings_vue/vue.py
+++ functions/settings_vue/vue.py
@@ -15,7 +15,7 @@ def run_dev(project):
     try:runSubprocess(f'cd {project} && npm run dev',shell=True, check=True)
     except CalledProcessError as cp: print(f'An error occurred: {cp.stderr}')
 def manage_vue():
-    print('\n\n*************************** SETTINGS VUE ***************************************');options=['1','2','3','4'];option='1'
+    print('\n\n*************************** SETTINGS VUE ***************************************');options=['1','2','3','4','5'];option='1'
     while option in options:
         option=input('\n1. npm install vue\n2. npm init vue@latest\n3. npm install\n4. npm run format\n5. npm run dev\n\nInput a choice: ')
         if option == '1': vue_install()
@@ -24,5 +24,5 @@ def manage_vue():
             project=input('Input the project name: '); project=osJoin(getcwd(), project)
             if option == '3': npm_install(project)
             elif option == '4': run_format(project)
-            elif option == '5': run_dev()
+            elif option == '5': run_dev(project)
         elif option not in options: print('************************************ END SETTINGS VUE *****************************************')
