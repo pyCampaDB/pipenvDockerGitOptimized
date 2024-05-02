90a541c0bb72ecff6af14c85429a2682d259dc85
pyCampaDB <rafacampamartinez@gmail.com>
2024-05-02 21:11:56 +0200

    first commit

--- functions/settings_vue/vue.py
+++ functions/settings_vue/vue.py
@@ -1,23 +1,28 @@
-from subprocess import CalledProcessError, run as runSubprocess
+from subprocess import CalledProcessError, run as runSubprocess;from os import getcwd; from os.path import join as osJoin
 def vue_install():
     try:runSubprocess('npm install vue', shell=True, check=True)
     except CalledProcessError as cp: print(f'An error occurred: {cp.stderr}')
 def vue_init():
     try:runSubprocess('npm init vue@latest', shell=True, check=True)
     except CalledProcessError as cp: print(f'An error occurred: {cp.stderr}')
+def npm_install(project):
+    try:runSubprocess(f'cd {project} && npm install', shell=True, check=True)
+    except CalledProcessError as cp:print(f'An error occurred: {cp.stderr}')
 def run_format(project):
-    try:runSubprocess(f'cd {project} && npm run format')
+    try:runSubprocess(f'cd {project} && npm run format', shell=True, check=True)
     except CalledProcessError as cp: print(f'An error occurred: {cp.stderr}')
 def run_dev(project):
-    try:runSubprocess(f'cd {project} && npm run dev')
+    try:runSubprocess(f'cd {project} && npm run dev',shell=True, check=True)
     except CalledProcessError as cp: print(f'An error occurred: {cp.stderr}')
 def manage_vue():
     print('\n\n*************************** SETTINGS VUE ***************************************');options=['1','2','3','4'];option='1'
     while option in options:
-        option=input('\n1. npm install vue\n2. npm init vue@latest\n3. npm run format\n4. npm run dev\n\nInput a choice: ')
+        option=input('\n1. npm install vue\n2. npm init vue@latest\n3. npm install\n4. npm run format\n5. npm run dev\n\nInput a choice: ')
         if option == '1': vue_install()
         elif option == '2': vue_init()
-        elif option in ['3', '4']:project=input('Input the project name: ')
-        if option == '3': run_format(project)
-        elif option == '4': run_dev(project)
+        elif option in ['3', '4','5']:
+            project=input('Input the project name: '); project=osJoin(getcwd(), project)
+            if option == '3': npm_install(project)
+            elif option == '4': run_format(project)
+            elif option == '5': run_dev()
         elif option not in options: print('************************************ END SETTINGS VUE *****************************************')
